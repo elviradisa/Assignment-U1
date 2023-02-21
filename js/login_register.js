9e33df19202bbbdb53540e0bbe2f476814a54bda
Elvira Ericsson <elvira.rigi@hotmail.com>
2023-02-21 09:04:02 +0100

    create login_register

---
+++ js/login_register.js
@@ -0,0 +1,10 @@
+function register () {
+    document.querySelector("#title_login").textContent = "REGISTER";
+    document.querySelector("#paragraph").textContent = "Ready when you are..."
+    document.querySelector(".login_button").textContent = "Register"
+    document.querySelector("#wrapper").style.backgroundColor = "#5a966a";
+}
+
+function return_to_login () {
+
+}
