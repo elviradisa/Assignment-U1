24f03146eb8ade5d24bf40a90d29a811807cb047
Elvira Ericsson <elvira.rigi@hotmail.com>
2023-02-26 23:00:28 +0100

    update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,10 +1,55 @@
-async function get_breeds_from_URL () {
-    document.querySelector(".background_contacting_server").style.display = "flex";
-    document.querySelector(".quiz_response").style.display = "flex";
+"use strict";
 
-    const image_request = new Request("https://teaching.maumt.se/apis/access/");
+let i = 0;
+
+async function check_answer (button_id) {
+
+    const get_breed = ALL_BREEDS[i];
+    const random_number = Math.floor(Math.random() * ALL_BREEDS.length);
+    const random_number2 = Math.floor(Math.random() * ALL_BREEDS.length);
+    const random_number3 = Math.floor(Math.random() * ALL_BREEDS.length);
+    
+    const get_breed2 = ALL_BREEDS[random_number];
+    const get_breed3 = ALL_BREEDS[random_number2];
+    const get_breed4 = ALL_BREEDS[random_number3];
+    
+    const image_request = new Request(`https://dog.ceo/api/breed/${get_breed.url}/images/random`);
     const response =  await send_request(image_request);
+
+    const data = await response.json();
+
+    document.querySelector(".standby_image").src = data.message; 
+    document.querySelector("#top_left").textContent = get_breed.name;
+    document.querySelector("#top_right").textContent = get_breed2.name;
+    document.querySelector("#bottom_left").textContent = get_breed3.name;
+    document.querySelector("#bottom_right").textContent = get_breed4.name;
+    document.querySelector(".response_answer").style.display = "flex";
+
+    if (button_id) {
+        if (document.querySelector(`#${button_id}`).textContent === get_breed.name) {
+            document.querySelector(".quiz_response").style.display = "flex";
+            document.querySelector(".response_answer").textContent = "Correct!"
+            document.querySelector(".quiz_response").style.backgroundColor = "#a9e8bf";
+            document.querySelector(".close_display").style.display = "flex";
+            document.querySelector(".background_quiz_response").style.display = "flex";
+            document.querySelector("#wrapper").style.overflow = "hidden";
+            document.querySelector("#wrapper").style.height = "100vh";
+            
+        } else {
+            document.querySelector(".quiz_response").style.display = "flex";
+            document.querySelector(".close_display").style.display = "flex";
+            document.querySelector(".response_answer").textContent = "I'm afraid not!"
+            document.querySelector(".quiz_response").style.backgroundColor = "#e69753";
+            document.querySelector(".background_quiz_response").style.display = "flex";
+            document.querySelector("#wrapper").style.overflow = "hidden";
+            document.querySelector("#wrapper").style.height = "100vh";
+        }
+
+    }
+    i += 1;
 }
+check_answer();
+
 
 function logout () {
     document.querySelector(".background_contacting_server").style.display = "none";
@@ -12,17 +57,17 @@ function logout () {
     document.querySelector(".login_register").style.display = "flex";
     document.querySelector(".status_code").style.display = "none";
     document.querySelector(".quiz").style.display = "none";
+    document.querySelector("#paragraph").textContent = "Let the magic start!"
+    document.querySelector("#paragraph").style.backgroundColor = "transparent";
+    document.querySelector("#wrapper").style.backgroundColor = "#438a9b";
+
+    localStorage.removeItem("username");
 }
 
-function stay_when_refresh () {
-    const my_content = document.querySelector(".quiz").value;
-    localStorage.setItem("my_content", my_content);
+function close_answer_display () {
+    document.querySelector(".background_quiz_response").style.display = "none";
+    document.querySelector(".close_display").style.display = "none";
+    document.querySelector(".quiz_response").style.display = "none";
+    document.querySelector("#wrapper").style.overflow = "scroll";
+    document.querySelector("#wrapper").style.height = "";
 }
-
-
-
-
-
-
-
-
